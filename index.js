fe6163e1c7bc9ecd8ea5c36562bf0606d16963c3
jun2theizzle <junnobaka@gmail.com>
2017-10-22 23:50:17 -0500

    added updateParty

--- index.js
+++ index.js
@@ -9,3 +9,16 @@ app.get('/', function(req, res){
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
+
+io.on('connection', function(socket) {
+  
+  socket.on('marco', function(data) {
+    io.emit('polo', 'You got stuff');
+  });
+
+  // Receives a emit from the app when DM updates a character
+  socket.on('updateParty', function(data) {
+    io.emit('getParty');
+  });
+
+});
