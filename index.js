e5d9b7341c7be7c36a43e0d1ce8ac994e5100151
EC2 Default User <ec2-user@ip-172-31-0-58.ec2.internal>
2018-03-19 13:58:12 +0000

    fixed ssl socket server

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-var https = require('http');
-var fs =    require('fs');
-var options = {
-      key:    fs.readFileSync('key.pem'),
-      cert:   fs.readFileSync('cert.pem'),
-      ca:     fs.readFileSync('ca.pem'),
-    };
-var app = https.createServer(options);
-io = require('socket.io').listen(app);
-app.listen(3000);
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+var privateKey  = fs.readFileSync('privkey.pem', 'utf8');
+var certificate = fs.readFileSync('cert.pem', 'utf8');
+var bundle = fs.readFileSync('ca.pem', 'utf8');
+var socketIO = require('socket.io');
 
+var credentials = {ca: bundle,  requestCert: true, key: privateKey, cert: certificate,   rejectUnauthorized: false };
+var express = require('express');
+var app = express();
+var cors = require('cors')
+app.use(cors())
+// your express configuration here
+
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(8080);
+httpsServer.listen(3000);
+                          
+
+
+
+var io = socketIO.listen(httpsServer);
 
 io.on('connection', function(socket) {
 
